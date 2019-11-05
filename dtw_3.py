336f71b92ef45b8e23ed2953b582d868b060156b
BrunoPicolo <picolobruno@hotmail.com>
2019-11-05 09:35:37 +0100

    Finie compare_mfcc

--- dtw_3.py
+++ dtw_3.py
@@ -7,24 +7,12 @@ import sys
 import numpy as np
 # from google.colab import drive
 
-
-def euclidienne( a, b ):
-    return abs( a - b ) 
-
-def lettres( a, b ):
-    return 0 if a==b else 1
-
-def sons( a, b, d ):
-    dsons = np.array( [[0,1,1,1,2],
-                       [1,0,1,1,2],
-                       [1,1,0,1,1],
-                       [1,1,1,0,2],
-                       [2,2,1,2,0]] )
-    return ( dsons[d[a]][d[b]] )
- 
-def sonsTD (a, b):
-    d = { 'X':0, 'C':1, 'U':2, 'Ux':3, 'V':4 }
-    return sons(a, b, d)
+# norm of two vector of the same size
+def norm( A, B ):
+    s = 0 
+    for i in range( len(A) ):
+        s += ( A[i] - B[i] )**2
+    return math.sqrt( s )
  
 def dtw( A, B, I, J, x ):
     w0 = 1
@@ -44,55 +32,22 @@ def dtw( A, B, I, J, x ):
                            g[i][j-1] + w2*d )
     return g[I-1][J-1] / ( I+J )
 
-# np.linalg.norm( fi, fg )
 def compare_mfcc( tFile, aFile ):
     y1, sr1 = librosa.load( tFile )
     y2, sr2 = librosa.load( aFile )
     
-    mfcc1 = np.array( librosa.feature.mfcc( y=y1, sr=sr1, hop_length=1024, htk=True, n_mfcc=12 ) ) 
-    mfcc2 = np.array( librosa.feature.mfcc( y=y2, sr=sr2, hop_length=1024, htk=True, n_mfcc=12 ) )
+    mfcct = np.array( librosa.feature.mfcc( y=y1, sr=sr1, hop_length=1024, htk=True, n_mfcc=12 ) ).transpose()
+    mfcca = np.array( librosa.feature.mfcc( y=y2, sr=sr2, hop_length=1024, htk=True, n_mfcc=12 ) ).transpose()
 
-    
-# TODO le contenu de dist est peut être pas bon 
+    dist = dtw( mfcct, mfcca, mfcct.shape[0], mfcca.shape[0], norm ) 
+    print( dist )
+
+
 # ------------------------------------------------------------------------------------- # 
-
-
-A = [ -2, 10, -10, 15, -13, 20, -5, 14, 2 ]
-B = [ 3, -13, 14, -7, 9, -2 ]
-
-s1 = [ 'X', 'C', 'U' ]
-s2 = [ 'X', 'C', 'U' ]
-
-obs = [ 'X', 'X', 'V', 'U', 'X', 'C', 'X']
-cinq = np.array( [ 'X', 'V', 'V','C','X'] )
-cent = np.array( [ 'X', 'V', 'V'] )
-vingth = ['Ux', 'V', 'V']
-
-z =  dtw( B, A, 6, 9, euclidienne)
-x1 = dtw( cinq, obs,  len(cinq), len(obs), sonsTD )
-x2 = dtw( cent, obs,  len(cent), len(obs), sonsTD )
-x3 = dtw( vingth, obs,  len(vingth),len(obs), sonsTD )
-
-print(z, x1, x2, x3)
 
 if ( len( sys.argv ) != 3 ):
     print( "Usage: $python3 %s <file1.wav> <file2.wav>" % sys.argv[0] )
     exit( -1 )
     
 print( "Comparing", sys.argv[1], sys.argv[2] )
-compare_mfcc( sys.argv[1], sys.argv[2] )
-	
-
-## Rapport : differents ponderations, couts ...
-## scénarios à comparer:
-#	-> bruite - nonbruite ( homme )
-#	-> femme - femme
-# 	-> homme - homme
-# 	-> home - femme
-
-# compare est une fonction de comparaison 
-# remplacer librosa.load et décomenter l'import de google colab
-# Ex y, sr = librosa.load("corpus/dronevolant_bruite/M01_arretetoi.wav", offset=30, duration=5)
-# https://stackoverflow.com/questions/6932096/matching-two-series-of-mfcc-coefficients
-# https://stackoverflow.com/questions/8433401/defining-a-matrix-norm-to-compare-two-mfcc-matrices
-
+compare_mfcc( sys.argv[1], sys.argv[2] )	
